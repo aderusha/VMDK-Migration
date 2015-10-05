4699eb184c061578d449d5d9f4286a9b0260e201
aderusha <allen@Thorium.derusha.net>
2015-10-05 11:19:08 -0400

    Whitespace cleanup

--- Export-VMDKs.ps1
+++ Export-VMDKs.ps1
@@ -1,13 +1,13 @@
 ﻿<#  
 .SYNOPSIS 
-    Export all VMDKs for a named Datacenter to a CSV
+	Export all VMDKs for a named Datacenter to a CSV
 .DESCRIPTION
-    Run this script to export a list of all Hard Disk objects assigned to all VMs
-    in the specified vSphere Datacenter.  The resulting CSV is ready for editing
-    to map out target datastore objects for a Storage vMotion script to be created
-    by Create-MigrationScript.ps1
+	Run this script to export a list of all Hard Disk objects assigned to all VMs
+	in the specified vSphere Datacenter.  The resulting CSV is ready for editing
+	to map out target datastore objects for a Storage vMotion script to be created
+	by Create-MigrationScript.ps1
 .NOTES
-    Author: Allen Derusha
+	Author: Allen Derusha
 #>
 
 ################################################################################
@@ -20,26 +20,26 @@ $strVCenterServer = "vcenter"
 # Include required VMware Snapins
 $snapin="VMware.VimAutomation.Core"
 if (Get-PSSnapin $snapin -ErrorAction "SilentlyContinue") {
-    # Write-Host "PSsnapin $snapin is loaded"
+	# Write-Host "PSsnapin $snapin is loaded"
 }
 elseif (Get-PSSnapin $snapin -registered -ErrorAction "SilentlyContinue") {
-    # Write-Host "PSsnapin $snapin is registered but not loaded"
-    Add-PSSnapin $snapin
+	# Write-Host "PSsnapin $snapin is registered but not loaded"
+	Add-PSSnapin $snapin
 }
 else {
-    throw "Required PSSnapin $snapin not found"
+	throw "Required PSSnapin $snapin not found"
 }
 
 # Connect to vCenter instance if we aren't already
 if ($global:DefaultVIServers.Count -lt 1) {
-    Connect-VIServer -Server $strVCenterServer -Protocol https
+	Connect-VIServer -Server $strVCenterServer -Protocol https
 }
 
 $myOutTable = @()
 
 ForEach ($myVMDK in Get-Datacenter -Name $strSourceDatacenter | Get-VM | Get-HardDisk | Sort-Object Parent, Name) { 
 	$myOutRecord = "" | Select-Object VMName, HDDName, Size, Filename, SourceDatastore, TargetDatastore
-    $myOutRecord.VMName = $myVMDK.Parent
+	$myOutRecord.VMName = $myVMDK.Parent
 	$myOutRecord.Filename = $myVMDK.Filename
 	$myOutRecord.Size = $myVMDK.CapacityGB
 	$myOutRecord.HDDName = $myVMDK.Name
